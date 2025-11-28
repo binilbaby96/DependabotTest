50ffdb3592e46a8fd47e48cb97c90f2cc6cc4f83
binilbaby96 <72088642+binilbaby96@users.noreply.github.com>
2025-11-28 12:38:43 +0530

    Add files via upload
    initial commit

---
+++ Dependabot/DependabotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependabotApp.swift
+//  Dependabot
+//
+//  Created by flock on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DependabotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DependabotTests/DependabotTests.swift
@@ -0,0 +1,17 @@
+//
+//  DependabotTests.swift
+//  DependabotTests
+//
+//  Created by flock on 28/11/25.
+//
+
+import Testing
+@testable import Dependabot
+
+struct DependabotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
