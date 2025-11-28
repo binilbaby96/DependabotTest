86f58b189b5c52fb64e1825a51d3b78c02507ef1
binilbaby96 <72088642+binilbaby96@users.noreply.github.com>
2025-11-28 13:58:45 +0530

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "iOSAppDependencies",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "iOSAppDependencies", targets: ["AppDependencies"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/pusher/NWWebSocket.git",
+            exact: "0.5.7"
+        ),
+        .package(
+            url: "https://github.com/pusher/pusher-websocket-swift.git",
+            exact: "10.1.5"
+        ),
+        .package(
+            url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap",
+            exact: "1.1.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "AppDependencies",
+            dependencies: [
+                .product(name: "NWWebSocket", package: "nwwebsocket"),
+                .product(name: "PusherSwift", package: "pusher-websocket-swift"),
+                .product(name: "TweetNacl", package: "tweetnacl-swiftwrap")
+            ],
+            path: "Sources"
+        )
+    ]
+)
